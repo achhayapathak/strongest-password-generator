cbb31ec35bcfd595022c99844fc096e0a8055803
achhayapathak <achhayapathak11@gmail.com>
2024-06-19 02:04:49 +0530

    Added count to generate multiple passwords

--- generatePassword.js
+++ generatePassword.js
@@ -1,4 +1,4 @@
-function generatePassword(length, includeUppercase, includeNumbers, includeSpecialChars) {
+function generatePassword(count, length, includeUppercase, includeNumbers, includeSpecialChars) {
 
     if (!length || typeof length === "string"){
      return new Error ("Please provide password length")     
@@ -10,18 +10,24 @@ function generatePassword(length, includeUppercase, includeNumbers, includeSpeci
     const specialChars = '!@#$%^&*()_-+=<>?/{}[]';
 
     let validChars = lowercaseChars;
-    let password = '';
+    let passwords = [];
 
     if (includeUppercase) validChars += uppercaseChars;
     if (includeNumbers) validChars += numberChars;
     if (includeSpecialChars) validChars += specialChars;
 
-    for (let i = 0; i < length; i++) {
-      const randomIndex = Math.floor(Math.random() * validChars.length);
-      password += validChars[randomIndex];
+    for (let i = 0; i < count; i++) {
+      let password = '';
+
+      for (let i = 0; i < length; i++) {
+        const randomIndex = Math.floor(Math.random() * validChars.length);
+        password += validChars[randomIndex];
+      }
+
+      passwords.push(password);
     }
 
-    return password;
+    return passwords;
   }
 
 module.exports = generatePassword

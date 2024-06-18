0c3160ae5fe17eacb559f0b674e0dea9f546d13e
achhayapathak <achhayapathak11@gmail.com>
2024-06-19 01:31:11 +0530

    Created password-generator package

---
+++ generatePassword.js
@@ -0,0 +1,27 @@
+function generatePassword(length, includeUppercase, includeNumbers, includeSpecialChars) {
+
+    if (!length || typeof length === "string"){
+     return new Error ("Please provide password length")     
+    }
+
+    const lowercaseChars = 'abcdefghijklmnopqrstuvwxyz';
+    const uppercaseChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    const numberChars = '0123456789';
+    const specialChars = '!@#$%^&*()_-+=<>?/{}[]';
+
+    let validChars = lowercaseChars;
+    let password = '';
+
+    if (includeUppercase) validChars += uppercaseChars;
+    if (includeNumbers) validChars += numberChars;
+    if (includeSpecialChars) validChars += specialChars;
+
+    for (let i = 0; i < length; i++) {
+      const randomIndex = Math.floor(Math.random() * validChars.length);
+      password += validChars[randomIndex];
+    }
+
+    return password;
+  }
+
+module.exports = generatePassword
